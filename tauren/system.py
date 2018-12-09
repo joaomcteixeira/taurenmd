550a66b644444092fe7ce5b072c4ce111903fd47
joaomcteixeira <joaomcteixeira@gmail.com>
2018-12-09 17:28:26 +0100

    added system

---
+++ tauren/system.py
@@ -0,0 +1,24 @@
+"""
+CONTAINS GENERAL AND SYSTEM VARIABLES
+
+Copyright © 2018-2019 Tauren-MD Project
+
+Contributors to this file:
+- João M.C. Teixeira (https://github.com/joaomcteixeira)
+
+Tauren-MD is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tauren-MD is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Tauren-MD. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+trajectory_types = (".xtc", ".nc", ".trr", ".h5", ".pdb", ".binpos", ".dcd")
+topology_types = (".pdb", ".cif")
