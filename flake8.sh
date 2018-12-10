2171c80cf02494937e0af799b6560bdb7f5b2acd
joaomcteixeira <joaomcteixeira@gmail.com>
2018-12-10 16:13:06 +0100

    updated flake8

--- flake8.sh
+++ flake8.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# W293 blank line contains whitespaces
+# W503 line break before binary operator, actually favoured by PEP8
+# -hang-closing, allows:
+#my_func(
+#    var1,
+#    var2,
+#    )
+
 clear
-flake8 --hang-closing --ignore=W293 $1
+flake8 --hang-closing --ignore=W293,W503 $1
