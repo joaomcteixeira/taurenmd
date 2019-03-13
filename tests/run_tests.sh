32911b69e45d15d47bb65b6b12ba6034f66269d9
joaomcteixeira <joaomcteixeira@gmail.com>
2019-03-13 20:16:35 +0100

    added test configs

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm *_
+rm _*
 rm *.csv
 rm plot*
 rm taurenmd.log
@@ -8,4 +8,23 @@ rm traj_OUTPUT.dcd
 
 source activate taurenmd
 
+# basic test with RMSDS using MDTraj
 python ../taurenmd.py -c test_1.json
+# basic test with RMSDs using MDAnalysis
+python ../taurenmd.py -c test_2.json
+# basic test with atom_selection selecting chain 1 - MDTraj
+python ../taurenmd.py -c test_3.json
+# basic test with atom_selection selecting chain 1 - MDAnalysis
+python ../taurenmd.py -c test_4.json
+# savetraj with atom and frame selection - MDTraj
+python ../taurenmd.py -c test_5.json
+# savetraj with atom and frame selection - MDAnalysis
+python ../taurenmd.py -c test_6.json
+# combined RMSDs with atom selection (chain) - MDTraj
+python ../taurenmd.py -c test_7.json
+# same as above with empty selection
+python ../taurenmd.py -c test_8.json
+# combined RMSDs with atom selection (chain) - MDAnalysis
+python ../taurenmd.py -c test_9.json
+# combined RMSDs with atom selection (resid) - MDAnalysis
+python ../taurenmd.py -c test_11.json
