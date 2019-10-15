47a7c13406eefe4477a435ab45e7f4d9da0e5533
joaomcteixeira <joaomcteixeira@gmail.com>
2019-10-14 23:32:09 -0400

    added bioplottemplates dep

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         ],
     python_requires='>=3, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
+        'bioplottemplates',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
         ],
     extras_require={
