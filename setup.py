0fbabf73807a153b71bb5b16e54d0f53e5d9e7e3
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-10-28 18:02:16 -0400

    comments.

--- setup.py
+++ setup.py
@@ -50,15 +50,8 @@ setup(
         'Environment :: Console',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        #'Programming Language :: Python :: Implementation :: CPython',
-        #'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
         'Topic :: Utilities',
         ],
     project_urls={
