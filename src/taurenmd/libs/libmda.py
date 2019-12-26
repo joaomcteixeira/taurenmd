a17dc04667c34c725f6553ed56e59d7d54ee1dcf
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-12-26 16:47:14 -0500

    documenting

--- src/taurenmd/libs/libmda.py
+++ src/taurenmd/libs/libmda.py
@@ -1,5 +1,15 @@
 """
-Input/Output functions using MDAnalysis.
+Functions that wrap around `MDAnalysis library`_.
+
+Functions contained in this module operate with MDAnalysis (MDA)
+functionalities, either by using MDA to access Molecular Dynamics
+data or by receiving MDA data structures and parsing it in some way.
+
+When using functions contained in this library you should `cite both`_
+taurenmd and MDAnalysis.
+
+.. _MDAnalysis library: https://www.mdanalysis.org
+.. _cite both: https://taurenmd.readthedocs.io/en/latest/citing.html
 """
 import MDAnalysis as mda
 
@@ -8,32 +18,68 @@ from taurenmd.libs import libio
 from taurenmd.logger import S, T
 
 
-def mda_load_universe(topology, *trajectories):
+def load_universe(topology, *trajectories):
     """
-    Load MDAnalysis universe.
+    Load MDAnalysis universe from topology and list of trajectories.
+    
+    Accepts MDAnalysis compatible `topology formats`_ and
+    `trajectory formats`_. Read further on the `MDAnalysis Universe`_.
+
+    .. _topology formats: https://www.mdanalysis.org/docs/documentation_pages/topology/init.html
+    .. _trajectory formats: https://www.mdanalysis.org/docs/documentation_pages/coordinates/init.html#supported-coordinate-formats
+    .. _MDAnalysis Universe: https://www.mdanalysis.org/docs/documentation_pages/core/universe.html?highlight=universe#core-object-universe-mdanalysis-core-universe
+
+    Examples
+    --------
+        
+        >>> libmda.load_universe('topology.pdb', 'trajectory.dcd')
+
+        >>> libmda.load_universe(
+                'topology.pdb',
+                'traj_part_1.xtc',
+                'traj_part_2.xtc',
+                Path('my', 'md', 'folder', 'traj_part_3.xtc'),
+                )
 
     Parameters
     ----------
-    top
-        Topology file.
+    topology : str or Path object
+        Path to topology file.
 
-    traj
-        Trajectory file.
+    trajectories* : str of Path objects
+        Paths to trajectory file(s). Trajectory files will be used
+        sequentially to create the Universe.
     
     Return
     ------
     MDAnalysis Universe
     """
     libio.report_input(topology, trajectories)
-
     universe = mda.Universe(topology, trajectories)
-
-    mda_report(universe)
+    report(universe)
     return universe
 
 
-def mda_report(universe):
-    """Report information about the Universe."""
+def report(universe):
+    """
+    Report information about the Universe.
+    
+    Example
+    -------
+
+        >>> u = libmda.load_universe('topology.pdb', 'trajectory.xtc')
+        >>> libmda.report(u)
+       
+    Parameters
+    ----------
+    universe : MDAnalysis Universe
+        `MDAnalysis universe <https://www.mdanalysis.org/docs/documentation_pages/core/universe.html?highlight=universe#core-object-universe-mdanalysis-core-universe>`_.
+
+    Returns
+    -------
+    None
+
+    """
     log.info(T('Reporting'))
     log.info(S('number of frames: {}', len(universe.trajectory)))
     log.info(S('number of atoms: {}', len(universe.atoms)))
@@ -41,8 +87,26 @@ def mda_report(universe):
 
 def draw_atom_label_from_atom_group(atom_group):
     """
-    Translate `atom_group` to list of representing strings
-    for each atom.
+    Translate MDAnalysis Atom Group to list of strings for each atom.
+
+    Strings represent each atom by SEGID.RESNUM|RESNAME.NAME,
+    for example carbon alpha of Cys 18 of chain A would:
+        
+        >>> A.18Cys.CA
+
+    This function is used by taurenmd for data representation
+    purporses.
+
+    Parameters
+    ----------
+    atom_group : Atom Group obj
+       `MDAnalysis Atom group <https://www.mdanalysis.org/docs/documentation_pages/core/groups.html?highlight=atom%20group#MDAnalysis.core.groups.AtomGroup>`_.
+
+    Returns
+    -------
+    list of strings
+        Containing the atom string representation for each atom in
+        the Atom Group.
     """
 
     labels = []
