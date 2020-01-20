41b78042866dff7b7f21f89a096aab98215bc672
joaomcteixeira <joaomcteixeira@gmail.com>
2020-01-20 00:31:44 -0500

    Bump version: 0.8.5 → 0.8.6

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ project = 'taurenmd'
 year = '2019'
 author = 'Joao MC Teixeira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.8.5'
+version = release = '0.8.6'
 
 todo_include_todos = True
 pygments_style = 'trac'
--- src/taurenmd/__init__.py
+++ src/taurenmd/__init__.py
@@ -23,7 +23,7 @@ import logging
 from taurenmd.core import Path  # noqa: F401
 from taurenmd.logger import DEBUGFILE, LOGFILE
 
-__version__ = '0.8.5'
+__version__ = '0.8.6'
 
 _BANNER = __doc__.format(__version__)
 
