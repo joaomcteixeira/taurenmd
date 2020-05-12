acff0c01f02d9202bad312695796d42d4ae54d99
joaomcteixeira <joaomcteixeira@gmail.com>
2020-05-12 11:46:31 -0400

    Bump version: 0.8.13 → 0.8.14

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ def _read(*names, **kwargs):
 
 setup(
     name='taurenmd',
-    version='0.8.13',
+    version='0.8.14',
     license='GNU GPLv2',
     description='A command-line interface for analysis routines in Molecular Dynamics data.',
     long_description='%s\n%s' % (
