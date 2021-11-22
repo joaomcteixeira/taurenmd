fd11e2a1ec2982421f0ec212e9d2e5c422f95fcc
joaomcteixeira <joaomcteixeira@gmail.com>
2021-11-22 18:29:17 +0100

    tests and upgrade calc torsion implementation

--- tests/test_libcalc.py
+++ tests/test_libcalc.py
@@ -2,6 +2,7 @@
 import MDAnalysis as mda
 import numpy as np
 import pytest
+from math import sqrt
 
 from taurenmd.libs import libcalc as lc
 
@@ -95,3 +96,60 @@ def test_calc_planes_angle(a1, b1, c1, a2, b2, c2, aunit, expected):
     """Test angle between planes."""
     result = lc.calc_planes_angle(a1, b1, c1, a2, b2, c2, aunit)
     assert abs(result - expected) < 0.00001
+
+
+@pytest.mark.parametrize(
+    'coords, expected',
+    [
+        (np.array([[1, 0, 0], [0, 0, 0], [0, 0, 1], [0, 1, 1]]), np.array([90], dtype=float)),  # noqa: E501
+        (np.array([[1, 0, 0], [0, 0, 0], [0, 0, 1], [0, 1, 1], [1, 1, 1]]), np.array([90, -90], dtype=float)),  # noqa: E501
+        (
+            np.array((
+                (1, 0, 0.000),
+                (0, 0, 0.000),
+                (0, 1, 0.000),
+                (-1, 1, 0.000),
+                (-1, 1, 1),
+                (0, 1, 1),
+                (0, 0, 0)
+                )),
+            np.array([-180, 90, -0, -45], dtype=float),
+            ),
+        (
+            np.array((
+                (1, 0, 0.000),
+                (0, 0, 0.000),
+                (0, 1, 0.000),
+                (-1, 1, 0.000),
+                (-1, 1, 1),
+                )),
+            np.array([-180, 90], dtype=float),
+            ),
+        ]
+    )
+def test_calculate_torsions(coords, expected):
+    """Tests torsion calculation."""
+    result = np.degrees(lc.calc_torsion_angles(coords))
+    assert np.all(np.equal(result, expected))
+
+
+@pytest.mark.parametrize(
+    'p1,p2,p3,p4,expected',
+    [
+        (
+            np.array([1, 0, 0]),
+            np.array([0, 0, 0]),
+            np.array([0, 1, 0]),
+            np.array([
+                [1, 1, 0],  # 0
+                [0, 1, 1],
+                [sqrt(1), 1, sqrt(1)],
+                ]),
+            np.array([0, -90, -45]),
+            )
+        ]
+    )
+def test_calculate_torsions_set(p1, p2, p3, p4, expected):
+    """Tests torsion calculation."""
+    result = np.degrees(lc.torsion_set(p1, p2, p3, p4))
+    assert np.all(np.equal(result, expected))
