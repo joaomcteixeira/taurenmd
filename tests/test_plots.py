f2cf72a17a1699a8f20a02b5b9f2a3d3add3855b
joaomcteixeira <joaomcteixeira@gmail.com>
2022-01-31 23:20:20 +0100

    adde more tests

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -69,6 +69,37 @@ def test_plotparams_2():
     return
 
 
+def test_plotparams_3():
+    """Test plotparams swapped arguments with multidimensional y-data."""
+    plotparams.plot(
+        list(range(50)),
+        np.arange(100).reshape(2, 50),
+        labels=['label 1', 'label 2'],
+        title='Title',
+        xlabel='data',
+        xticks=list(range(50)),
+        xticks_labels=list(map(str, range(50))),
+        yticks=list(range(10)),
+        yticks_labels=list(map(str, range(10))),
+        hline=0.5,
+        ylabel='observable',
+        colors=['green', 'blue'],
+        xmax=50,
+        xmin=20,
+        ymax=50,
+        ymin=20,
+        grid=False,
+        legend=False,
+        vert_lines=(1, 10),
+        filename='this_plot.png',
+        )
+
+    # py37 compatibility
+    assert Path('this_plot.png').exists()
+    Path('this_plot.png').unlink()
+    return
+
+
 def test_labeldots_0():
     """Test label dots defaults."""
     labeldots.plot(
